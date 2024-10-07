286b1b0d5cd0321c55323c353d51e3eb33fc76e8
TobiasgutPrivat <tobias.gut.03@gmail.com>
2024-10-07 11:36:24 +0200

    Flet testing

--- __Main__.py
+++ __Main__.py
@@ -1,21 +1,7 @@
-import flet as ft
-
-def main(page: ft.Page):
-    # Navigation handler
-    def go_to_page2(e):
-        page.controls.clear()  # Clear the existing page content
-        page.add(ft.Text("Welcome to Page 2!"))
-        page.add(ft.ElevatedButton(text="Go to Page 1", on_click=go_to_page1))
-        page.update()  # Refresh the UI
-    
-    def go_to_page1(e):
-        page.controls.clear()  # Clear the existing page content
-        page.add(ft.Text("Welcome to Page 1!"))
-        page.add(ft.ElevatedButton(text="Go to Page 2", on_click=go_to_page2))
-        page.update()  # Refresh the UI
-    
-    # Start with Page 1
-    go_to_page1(None)
+from WorkBenchUI.WorkBenchLayout import main
+from typing import Callable
+import flet as ft # type: ignore
 
 # Run the app
-ft.app(target=main)
+target: Callable = main
+ft.app(target)
