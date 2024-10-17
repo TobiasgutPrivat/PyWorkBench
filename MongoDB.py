28ec2f4c873b4651231b19322e268ae3033aa6ec
TobiasgutPrivat <tobias.gut.03@gmail.com>
2024-10-17 15:14:27 +0200

    use dill

--- MongoDB.py
+++ MongoDB.py
@@ -1,18 +1,18 @@
 from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
-import pickle
+import dill # extended pickle functionality
 
 client: MongoClient = MongoClient("mongodb://localhost:27017/")
 WorkBenchDB: Database = client["PyWorkBench"]
 Objects: Collection = WorkBenchDB["Objects"]
 
 def createNewObject(obj: object) -> str:
-    return Objects.insert_one({"_obj": pickle.dumps(obj)}).inserted_id
+    return Objects.insert_one({"_obj": dill.dumps(obj)}).inserted_id
 
 def updateObject(id: str, obj: object):
-    objdict = pickle.dumps(obj)
-    Objects.update_one({"_id": id}, {"$set": {"_obj": objdict}})
+    Objects.update_one({"_id": id}, {"$set": {"_obj": dill.dumps(obj)}})
 
 def getObject(id: str) -> object:
-    return pickle.loads(Objects.find_one({"_id": id})["_obj"]) #type: ignore
+    return dill.loads(Objects.find_one({"_id": id})["_obj"]) #type: ignore
+    # pickle.loads can have issues when required packages aren't loaded
